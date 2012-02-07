796da43c4ab481c9fcf23fcceaaee06a87837cea
Mark McWilliams <mark@blep.net>
2012-02-06 21:12:55 -0800

    Added iTach class; created discover module function

--- itach.py
+++ itach.py
@@ -3,8 +3,32 @@
 import socket
 import struct
 import sys
+import re
 
-def listen():
+class iTach(object):
+    def __init__(self, ip_address):
+        self.ip_address = ip_address
+
+    def send_command(self, command):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self.ip_address, 4998))
+
+        s.send(command)
+        s.send('\r')
+
+        while True:
+            data = s.recv(1024)
+            print(data)
+            if data.endswith('\r'): break
+
+def discover():
+    p = re.compile((r'AMXB<-UUID=GlobalCache_(?P<UUID>.{12}).+'
+        r'Model=iTach(?P<Model>.+?)>.+'
+        r'Revision=(?P<Revision>.+?)>.+'
+        r'Config-URL=http://(?P<IP>.+?)>.+'
+        r'PCB_PN=(?P<PN>.+?)>.+'
+        'Status=(?P<Status>.+?)>'))
+
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind(('', 9131))
 
@@ -14,19 +38,16 @@ def listen():
 
     while True:
         data = s.recv(1024)
-        print('> %s' % data)
-
-def send_command(command):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(('itach.blep.net', 4998))
-
-    s.send(command)
-    s.send('\r')
-
-    while True:
-        data = s.recv(1024)
-        print(data)
-        if data.endswith('\r'): break
+        match = p.match(data)
+        if match:
+            itach = iTach(match.group('IP'))
+            itach.uuid = match.group('UUID')
+            itach.model = match.group('Model')
+            itach.revision = match.group('Revision')
+            itach.part_number = match.group('PN')
+            itach.status = match.group('Status')
+            return itach
 
 if __name__ == '__main__':
-    send_command(sys.argv[1])
+    itach = iTach('itach.blep.net')
+    itach.send_command(sys.argv[1])
